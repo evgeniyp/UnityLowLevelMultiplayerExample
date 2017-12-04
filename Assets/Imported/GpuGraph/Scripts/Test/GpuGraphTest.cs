d74fc83c0f0c313d827da5161afe72eaf300cbcf
Петухов Евгений Евгеньевич <Evgeny_Petukhov@epam.com>
2017-12-04 13:14:26 +0300

    GpuGraph asset imported

---
+++ Assets/Imported/GpuGraph/Scripts/Test/GpuGraphTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GpuGraphTest : MonoBehaviour
+{
+    private Rect GraphRect;
+    private int _count;
+
+    void Start()
+    {
+        var xSize = 0.5f * Screen.width;
+        var ySize = 0.2f * Screen.height;
+        var xPos = 0.2f * Screen.width;
+        var yPos = 0.0f * Screen.height + 10;
+
+        GraphRect = new Rect(xPos, yPos, xSize, ySize);
+    }
+
+    void Update()
+    {
+        _count++;
+
+
+        if(GraphManager.Graph != null)
+        {
+            //GraphManager.Graph.Plot("Test_WorldSpace", currentDeltaTime, Color.green, new GraphManager.Matrix4x4Wrapper(transform.position, transform.rotation, transform.localScale));
+            GraphManager.Graph.Plot("Test_ScreenSpace", Time.deltaTime, Color.green, GraphRect);
+            GraphManager.Graph.Plot("Count", _count, Color.red, GraphRect);
+        }
+    }
+}
