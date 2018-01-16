7809b490841f0817f2efe934e1cd89ad32d7c337
Петухов Евгений Евгеньевич <Evgeny_Petukhov@epam.com>
2018-01-16 10:22:04 +0300

    Sandbox scene

---
+++ Assets/Scripts/SandboxPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandboxPlayer : MonoBehaviour
+{
+    private Vector2 UserInput;
+
+    private void Update()
+    {
+        UserInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.back, UserInput.x * Time.deltaTime * Consts.ClientSpeed);
+        transform.Translate(Vector3.up * UserInput.y * Time.deltaTime * Consts.ClientSpeed);
+    }
+}
